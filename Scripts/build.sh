3a392f91e67cb838cdc463eb473226ee85cea58d
zacswin <42161765+zacswin@users.noreply.github.com>
2018-10-07 13:53:07 +1100

    Update build.sh

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -34,18 +34,4 @@ rc1=$?
 echo "Build logs (Windows)"
 cat $(pwd)/unity.log
 
-echo "Attempting build of ${UNITYCI_PROJECT_NAME} for OSX"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-	-batchmode \
-	-nographics \
-	-silent-crashes \
-	-logFile $(pwd)/unity.log \
-	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
-	-buildOSXUniversalPlayer "$(pwd)/Build/osx/${UNITYCI_PROJECT_NAME}.app" \
-	-quit
-
-rc2=$?
-echo "Build logs (OSX)"
-cat $(pwd)/unity.log
-
 exit $(($rc1|$rc2))
