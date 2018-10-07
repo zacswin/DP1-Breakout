6d31b12ac16d36b5d3fdadf6ea4896c4d3549f92
zacswin <102091051@student.swin.edu.au>
2018-10-07 14:16:38 +1100

    add test test class

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -18,19 +18,19 @@ cat $(pwd)/test.xml
 # exit if tests failed
 if [ $rc0 -ne 0 ]; then { echo "Failed unit tests"; exit $rc0; } fi
 
-## Make the build
-echo "Attempting build of ${UNITYCI_PROJECT_NAME} for OSX"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-	-batchmode \
-	-nographics \
-	-silent-crashes \
-	-logFile $(pwd)/unity.log \
-	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
-	-buildOSXUniversalPlayer "$(pwd)/Build/osx/${UNITYCI_PROJECT_NAME}.app" \
-	-quit
+# ## Make the build
+# echo "Attempting build of ${UNITYCI_PROJECT_NAME} for OSX"
+# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+# 	-batchmode \
+# 	-nographics \
+# 	-silent-crashes \
+# 	-logFile $(pwd)/unity.log \
+# 	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
+# 	-buildOSXUniversalPlayer "$(pwd)/Build/osx/${UNITYCI_PROJECT_NAME}.app" \
+# 	-quit
 
-rc1=$?
-echo "Build logs (OSX)"
-cat $(pwd)/unity.log
+# rc1=$?
+# echo "Build logs (OSX)"
+# cat $(pwd)/unity.log
 
-exit $rc1
+# exit $rc1
