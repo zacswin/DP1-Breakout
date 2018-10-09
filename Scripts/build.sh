a62e0a28ce5f9ddcf8ecc0d7bcea7b05dab5d524
zacswin <102091051@student.swin.edu.au>
2018-10-09 16:11:38 +1100

    attempt to add builds back

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,38 +1,16 @@
-#! /bin/sh
-
-## Run the editor unit tests
-echo "Running editor unit tests for ${UNITYCI_PROJECT_NAME}"
+## Make the build
+echo "Attempting build of ${UNITYCI_PROJECT_NAME} for OSX"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
 	-batchmode \
 	-nographics \
 	-silent-crashes \
 	-logFile $(pwd)/unity.log \
 	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
-	-runEditorTests \
-	-editorTestsResultFile $(pwd)/test.xml \
+	-buildOSXUniversalPlayer "$(pwd)/Build/osx/${UNITYCI_PROJECT_NAME}.app" \
 	-quit
 
-rc0=$?
-echo "Unit test logs"
-cat $(pwd)/test.xml
-# exit if tests failed
-if [ $rc0 -ne 0 ]; then { echo "Failed unit tests"; exit $rc0; } fi
+rc1=$?
+echo "Build logs (OSX)"
+cat $(pwd)/unity.log
 
-exit 0
-
-# ## Make the build
-# echo "Attempting build of ${UNITYCI_PROJECT_NAME} for OSX"
-# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
-# 	-batchmode \
-# 	-nographics \
-# 	-silent-crashes \
-# 	-logFile $(pwd)/unity.log \
-# 	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
-# 	-buildOSXUniversalPlayer "$(pwd)/Build/osx/${UNITYCI_PROJECT_NAME}.app" \
-# 	-quit
-
-# rc1=$?
-# echo "Build logs (OSX)"
-# cat $(pwd)/unity.log
-
-# exit $rc1
+exit $rc1
