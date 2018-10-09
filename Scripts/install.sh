47167c99c91987213f09a77d1948be11526606f6
zacswin <42161765+zacswin@users.noreply.github.com>
2018-10-09 23:23:25 +1100

    Update install.sh

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 echo 'Downloading Unity 2017.3.1f pkg:'
-curl --retry 5 -o Unity.pkg https://download.unity3d.com/download_unity/a9f86dcd79df/MacEditorInstaller/Unity-2017.3.0f3.pkg
+curl --retry 5 -o Unity.pkg https://download.unity3d.com/download_unity/46dda1414e51/MacEditorInstaller/Unity-2017.2.0f3.pkg
 if [ $? -ne 0 ]; then { echo "Download failed"; exit $?; } fi
 
 # Run installer(s)
