27506e57d38525b74d43e92a62bd340dddee00cb
zacswin <102091051@student.swin.edu.au>
2018-10-10 12:28:34 +1100

    add test test

---
+++ Breakout/Assets/Editor/TestTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class TestTest {
+
+	[Test]
+	public void TestTestSimplePasses() {
+		Assert.AreEqual(10, 5 + 5);
+	}
+
+	// A UnityTest behaves like a coroutine in PlayMode
+	// and allows you to yield null to skip a frame in EditMode
+	[UnityTest]
+	public IEnumerator TestTestWithEnumeratorPasses() {
+		// Use the Assert class to test conditions.
+		// yield to skip a frame
+		yield return null;
+	}
+}
